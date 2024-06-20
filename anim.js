2c72432ae254a4d0694b6ce98c51f22aa2d60ceb
thomas.fleury73 <thomas.fleury73@gmail.com>
2024-06-20 10:21:45 +0200

    DEV : CSS

---
+++ anim.js
@@ -0,0 +1,33 @@
+/* ========================================================================= *\
+**  === SCROLL REVEAL                                                        *| 
+\* ========================================================================= */
+
+  ScrollReveal().reveal('.reveal', {
+    interval: 0,
+    duration: 1000,
+  });
+
+  ScrollReveal().reveal('.reveal--top', {  
+    origin: 'top',
+    distance: '50px',
+  });
+
+  ScrollReveal().reveal('.reveal--bottom', {  
+    origin: 'bottom',
+    distance: '50px',
+  });
+
+  ScrollReveal().reveal('.reveal--left', {  
+    origin: 'left',
+    distance: '50px',
+  });
+
+  ScrollReveal().reveal('.reveal--right', {  
+    origin: 'right',
+    distance: '50px',
+  });
+  
+  ScrollReveal().reveal('.reveal--reset', { 
+    reset: true,
+  });
+
