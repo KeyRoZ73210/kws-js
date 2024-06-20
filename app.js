a1d4df338aa371f6c3248110d5c6cb95b4924caf
Aurelien <aurelien.fillion.pro@gmail.com>
2024-06-20 15:07:46 +0200

    build: debut du rec vocal

--- app.js
+++ app.js
@@ -1,108 +1,139 @@
-/**
- * Initializes the audio context for the browser.
- * @returns {AudioContext} The initialized audio context.
- */
-function getAudioContext() {
-    return new (window.AudioContext || window.webkitAudioContext)();
-}
-
-/**
- * Asynchronously loads the TensorFlow model from a specified path.
- * @returns {Promise<tf.GraphModel>} The loaded TensorFlow model.
- */
-async function loadModel() {
-    const modelPath = 'tfjs_model/model.json';
-    return await tf.loadGraphModel(modelPath);
-}
-
-/**
- * Reads an audio file and decodes it into an AudioBuffer.
- * @param {File} file The audio file to read.
- * @returns {Promise<AudioBuffer>} The decoded AudioBuffer.
- */
-async function decodeAudioFile(file) {
-    const arrayBuffer = await file.arrayBuffer();
-    const audioContext = getAudioContext();
-    return await audioContext.decodeAudioData(arrayBuffer);
-}
-
-/**
- * Processes the audio buffer to match the input requirements of the TensorFlow model.
- * @param {AudioBuffer} audioBuffer The audio buffer to process.
- * @returns {tf.Tensor3D} The processed audio as a 3D tensor.
- */
-function processAudioBuffer(audioBuffer) {
-    const targetSampleRate = 16000;
-    const inputLength = 16000;
-    const offlineContext = new OfflineAudioContext(
-        audioBuffer.numberOfChannels,
-        audioBuffer.duration * targetSampleRate,
-        targetSampleRate
-    );
-
-    const bufferSource = offlineContext.createBufferSource();
-    bufferSource.buffer = audioBuffer;
-    bufferSource.connect(offlineContext.destination);
-    bufferSource.start(0);
-
-    return offlineContext.startRendering().then(renderedBuffer => {
-        let inputData = renderedBuffer.getChannelData(0);
-        if (inputData.length > inputLength) {
-            inputData = inputData.slice(0, inputLength);
-        } else if (inputData.length < inputLength) {
-            // Fill the remaining input with zeros if the audio is too short
-            inputData = new Float32Array(inputLength).fill(0, 0, inputLength);
-            inputData.set(renderedBuffer.getChannelData(0));
-        }
-        return tf.tensor3d(inputData, [1, inputLength, 1]);
-    });
-}
-
-
-/**
- * Handles the TensorFlow model predictions by displaying the most likely prediction in the UI.
- * @param {tf.Tensor} outputTensor The output tensor from the model prediction.
- */
-async function handlePrediction(outputTensor) {
-    const prediction = await outputTensor.data();
-    const highestPrediction = labels.map((label, index) => ({
-        label: label, probability: prediction[index]
-    })).sort((a, b) => b.probability - a.probability)[0];
-
-    outputTensor.dispose();
-    return { label: highestPrediction.label, probability: highestPrediction.probability };
-}
-
-/**
- * Orchestrates the loading, processing, and predicting of an audio file.
- */
-async function loadAndPredict() {
-    const fileInput = document.getElementById('audioUpload');
-    const audioFile = fileInput.files[0];
-    if (!audioFile) {
-        console.log('Please upload an audio file.');
-        return;
-    }
-
-    try {
-        const startTime = performance.now();
-
-        const audioBuffer = await decodeAudioFile(audioFile);
-        const inputTensor = await processAudioBuffer(audioBuffer);
-        const model = await loadModel();
-        const outputTensor = await model.executeAsync(inputTensor);
-        const result = await handlePrediction(outputTensor);
-
-        const endTime = performance.now();
-        const inferenceTime = endTime - startTime;
-
-        document.getElementById('predictionOutput').textContent = `Prediction: ${result.label} with probability ${result.probability.toFixed(2)} in ${inferenceTime.toFixed(2)} ms`;
-    } catch (error) {
-        console.error('Error processing audio file:', error);
-    }
-}
+let audioContext;
+let recorder;
+let audioStream;
+let recordedChunks = [];
 
 const labels = [
-    "background_noise", "forward", "groovit", "happy", "nine",
-    "surpuissant", "two", "visual", "yes"
+  "background_noise",
+  "forward",
+  "groovit",
+  "happy",
+  "nine",
+  "surpuissant",
+  "two",
+  "visual",
+  "yes",
 ];
+
+const recordButton = document.getElementById("recordButton");
+recordButton.addEventListener("click", toggleRecording);
+
+async function toggleRecording() {
+  if (recorder && recorder.state === "recording") {
+    recorder.stop();
+    stopAudioStream();
+    recordButton.textContent = "Appuyer pour enregistrer";
+  } else {
+    await startRecording();
+    recordButton.textContent = "Arrêter l'enregistrement";
+  }
+}
+
+async function startRecording() {
+  try {
+    if (!audioContext) {
+      audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    }
+    if (!audioStream) {
+      audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    }
+    if (!recorder) {
+      recorder = new MediaRecorder(audioStream);
+      recorder.ondataavailable = (event) => recordedChunks.push(event.data);
+      recorder.onstop = processAudio;
+    }
+    recordedChunks = [];
+    recorder.start();
+  } catch (error) {
+    console.error("Error startin recording:", error);
+  }
+}
+
+function stopAudioStream() {
+  if (audioStream) {
+    audioStream.getTracks().forEach((track) => track.stop());
+    audioStream = null;
+  }
+}
+
+function processAudio() {
+  const audioBlob = new Blob(recordedChunks, { type: "audio/webm" });
+  const reader = new FileReader();
+  reader.onload = async (event) => {
+    const arrayBuffer = event.target.result;
+    try {
+      const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+      const audioData = audioBuffer.getChannelData(0);
+
+      // Utiliser TensorFlow.js pour la prédiction ici
+      await loadAndPredict(audioData);
+    } catch (error) {
+      console.error("Error decoding audio data:", error);
+    } finally {
+      if (audioContext) {
+        audioContext.close();
+        audioContext = null;
+      }
+    }
+  };
+  reader.readAsArrayBuffer(audioBlob);
+}
+
+async function loadAndPredict(audioData) {
+  // Charger le modèle TensorFlow.js
+  const model = await tf.loadGraphModel("tfjs-model/model.json");
+
+  // Préparer les données pour la prédiction
+  const inputTensor = tf.tensor(audioData.slice(0, 16000), [1, 16000, 1]);
+
+  // Faire une prédiction
+  const inputs = { input: inputTensor };
+  const outputs = await model.executeAsync(inputs);
+
+  // Assurez-vous que `outputs` est un tenseur ou une liste de tenseurs
+  let prediction;
+  if (outputs instanceof tf.Tensor) {
+    prediction = outputs.dataSync();
+  } else if (Array.isArray(outputs)) {
+    prediction = outputs[0].dataSync();
+  } else {
+    throw new Error("Unexpected output format from model prediction.");
+  }
+
+  const predictedLabelIndex = prediction.indexOf(Math.max(...prediction));
+  const predictedLabel = labels[predictedLabelIndex];
+
+  // Afficher le résultat de la prédiction
+  document.getElementById(
+    "recognizedWord"
+  ).textContent = `Le mot reconnu est : ${predictedLabel}`;
+
+  // Utiliser l'API Web Speech pour répéter le mot reconnu
+  const utterance = new SpeechSynthesisUtterance(predictedLabel);
+  window.speechSynthesis.speak(utterance);
+}
+
+// Fonctionnalité existante
+async function predict() {
+  const audioUpload = document.getElementById("audioUpload");
+  if (audioUpload.files.length === 0) {
+    alert("Please upload a file");
+    return;
+  }
+  const file = audioUpload.files[0];
+  const arrayBuffer = await file.arrayBuffer();
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+  const audioData = audioBuffer.getChannelData(0);
+  await loadAndPredict(audioData);
+}
+
+// Charger l'audio et faire une prédiction pour le téléchargement de fichier
+async function decodeAudioFile(file) {
+  const arrayBuffer = await file.arrayBuffer();
+  const audioContext = getAudioContext();
+  return await audioContext.decodeAudioData(arrayBuffer);
+}
+
+function getAudioContext() {
+  return new (window.AudioContext || window.webkitAudioContext)();
+}
